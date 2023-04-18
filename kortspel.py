d690d8c053be2bc4ab689e21d3e65f97efbb1436
NTIEmil <emil.winroth@elev.ga.ntig.se>
2023-04-18 14:37:02 +0200

    Update kortspel.py

--- kortspel.py
+++ kortspel.py
@@ -1,1 +1,50 @@
 import pygame
+
+def variables():
+    global backgroundColour
+    global windowWidth
+    global windowHeight
+    global windowName
+    global playerHand
+
+    backgroundColour = (200, 200, 200)
+    windowWidth = 800
+    windowHeight = 600
+    windowName = 'UNO'
+    playerHand = {
+        [0, 'Green'],
+        [2, 'Yellow'],
+        [9, 'Blue']
+        [8, 'Red']
+    }
+
+def displayCards():
+    global playerHand
+
+    for i in len(playerHand):
+        pass
+        #Dispaly card side by side with the number in the middle.
+
+def start():
+    variables()
+    global backgroundColour
+    global windowWidth
+    global windowHeight
+    global windowName
+
+    screen = pygame.display.set_mode((windowWidth, windowHeight))
+    pygame.display.set_caption(windowName)
+    screen.fill(backgroundColour)
+
+    pygame.display.flip()
+
+    running = True
+
+    while running:
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                running = False
+
+start()
